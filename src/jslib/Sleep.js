fe637977c987e371c68755f70cfcc34d52b47889
xiguawannian <14610602+xiguawannian@user.noreply.gitee.com>
2024-07-28 16:23:18 +0800

    打字机

--- src/jslib/Sleep.js
+++ src/jslib/Sleep.js
@@ -1,6 +1,11 @@
+/**
+ * @description: 暂停一段时间
+ * @param number milliSeconds
+ * @return {*}
+ */
 export function sleep(milliSeconds) {
-        var startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + milliSeconds) {
-            console.log(new Date().getTime());
-        }//暂停一段时间 10000=1S。
-    }
+    return new Promise(resolve => {
+        setTimeout(resolve, milliSeconds);
+    });
+    }//暂停一段时间 1000=1S。
+    //需要用async和await
